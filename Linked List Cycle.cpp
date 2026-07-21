db01448e237e95fe64da91538c499fdd7b21134a
Shamim Ahmed Raju <140370926+ShamimAhmed510@users.noreply.github.com>
2026-07-21 23:41:40 +0600

    Create Linked List Cycle.cpp

---
+++ Linked List Cycle.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/linked-list-cycle/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
